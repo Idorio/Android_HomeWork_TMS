005b6f43b905ec6119e3c31e411a883419215125
Idorio <insaider9218@gmail.com>
2022-11-17 17:39:53 +0300

    Android Project HomeWork Kotlin part 2

--- app/src/main/java/com/example/kotlinhomework1/MainActivity.kt
+++ app/src/main/java/com/example/kotlinhomework1/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.kotlinhomework1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.kotlinhomework1.MainActivity2.Companion.startMainActivity2
+import com.example.kotlinhomework1.MainActivity2.Companion.statrtMainActivitygo
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.item_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.setting -> startMainActivity2(
+                this,
+                getString(R.string.hello_world),
+                getString(R.string.hello_children)
+            )
+            R.id.setting1 -> statrtMainActivitygo(
+                this,
+                getString(R.string.hello_children)
+            )
+            R.id.setting2 -> onBackPressedDispatcher
+            R.id.setting3 -> finishAffinity()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
+
+
+
+
+
+
